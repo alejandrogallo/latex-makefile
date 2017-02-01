12bca561792b24bde46cfb039c58aade771375a7
Alejandro Gallo <aamsgallo@gmail.com>
2017-02-01 17:17:57 +0100

    Refix git config

--- tests/diff/doTest.sh
+++ tests/diff/doTest.sh
@@ -8,10 +8,12 @@ eval $(make --no-print-directory print-DIFF_SRC_NAME)
 eval $(make --no-print-directory MAIN_SRC=${DIFF_BUILD_DIR}/${DIFF_SRC_NAME} print-TOC_FILE)
 eval $(make --no-print-directory MAIN_SRC=${DIFF_BUILD_DIR}/${DIFF_SRC_NAME} print-TOC_DEP)
 
+
+git init
+
 git config user.email "me@latex-makefile.com"
 git config user.name "Test job"
 
-git init
 git add .
 git commit -m "Initial commit"
 
