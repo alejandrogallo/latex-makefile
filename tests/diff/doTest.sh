74a41270ea393e27222d77b832dbc1b1078cc3e1
Alejandro Gallo <aamsgallo@gmail.com>
2017-02-01 16:38:57 +0100

    Update doTest

--- tests/diff/doTest.sh
+++ tests/diff/doTest.sh
@@ -1,42 +1,53 @@
-#cp ../../dist/Makefile .
+cp ../../dist/Makefile .
 
 TEST_DESCRIPTION="Check that latexdiff works with git commits"
 
-eval $(make --no-print-directory print-TOC_FILE)
-eval $(make --no-print-directory print-TOC_DEP)
 eval $(make --no-print-directory print-MAIN_SRC)
+eval $(make --no-print-directory print-DIFF_BUILD_DIR)
+eval $(make --no-print-directory print-DIFF_SRC_NAME)
+eval $(make --no-print-directory MAIN_SRC=${DIFF_BUILD_DIR}/${DIFF_SRC_NAME} print-TOC_FILE)
+eval $(make --no-print-directory MAIN_SRC=${DIFF_BUILD_DIR}/${DIFF_SRC_NAME} print-TOC_DEP)
 
 git init
 git add .
 git commit -m "Initial commit"
 
-cat >> ${MAIN_SRC}<<EOF
-Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
-vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-no sea takimata sanctus est Lorem ipsum dolor sit amet.
+sed -i -e \
+  "s/.*test-include.*/Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod/"  \
+  -i -e \
+  "s/sed/I do not like SED/g"  \
+  ${MAIN_SRC}
 
-\\end{document}
-EOF
-
+cat ${MAIN_SRC}
 git commit -am "Second commit"
+git log | cat
 
-
-make --no-print-directory QUIET=1 VIEW_PDF= diff
+make --no-print-directory QUIET=0 QQUIET= VIEW_PDF= diff
 
 rm -rf .git
 git checkout ${MAIN_SRC}
 
-exit 0
 
 
 
 
-if [[ ! -f "${TOC_DEP}" ]]; then
-  echo "${TOC_DEP} file not found!"
-  TEST_RESULT=1
-elif [[ ! -f "${TOC_FILE}" ]]; then
+if [[ ! -f "${DIFF_BUILD_DIR}/${TOC_FILE}" ]]; then
   echo "${TOC_FILE} file not found!"
+  TEST_RESULT=1
+elif [[ ! -f "${DIFF_BUILD_DIR}/$(basename ${DIFF_SRC_NAME} .tex).pdf" ]]; then
+  echo "$(basename ${DIFF_SRC_NAME} .tex).pdf file  not found!"
+  TEST_RESULT=1
+elif [[ ! -f "${DIFF_BUILD_DIR}/${DIFF_SRC_NAME}" ]]; then
+  echo "${DIFF_BUILD_DIR}/${DIFF_SRC_NAME} file not found!"
+  TEST_RESULT=1
+elif [[ ! -d "${DIFF_BUILD_DIR}" ]]; then
+  echo "${DIFF_BUILD_DIR} directory not found!"
+  TEST_RESULT=1
+elif [[ -f "${DIFF_SRC_NAME}" ]]; then
+  echo "${DIFF_BUILD_DIR} file  found, this should not be the case!"
+  TEST_RESULT=1
+elif [[ -f "$(basename ${DIFF_SRC_NAME} .tex).pdf" ]]; then
+  echo "$(basename ${DIFF_SRC_NAME} .tex).pdf file  found, this should not be the case!"
   TEST_RESULT=1
 else
   TEST_RESULT=0
